ba58d7a4c13f9302efb152fcd343891efbc4afa9
Molt3nZaX <ishkaa803@gmail.com>
2024-09-19 10:30:48 +0300

    updated pom.xml

--- src/main/java/demoqa/utils/DataConstantsUtils.java
+++
@@ -1,7 +0,0 @@
-package demoqa.utils;
-
-public interface DataConstantsUtils {
-    String PATH_TO_FIRST_TABLE_TEST_DATA = "src/test/resources/FirstTableTestData.json";
-    String PATH_TO_SECOND_TABLE_TEST_DATA = "src/test/resources/SecondTableTestData.json";
-    int MAX_SCROLL = 700;
-}
